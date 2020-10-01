5af7604415ab78f306bec6e97066b4979ebc837d
Aki <71226118+hyve-jpg@users.noreply.github.com>
2020-10-01 14:26:01 +0200

    added more levels to load

--- SpelMotor/IronWrought/Source/Game/Game.cpp
+++ SpelMotor/IronWrought/Source/Game/Game.cpp
@@ -40,7 +40,7 @@ CGame::~CGame()
 void CGame::Init()
 {
 	myLevelLoader->Init();
-	myLevelLoader->LoadNewLevel("Levels/SampleScene_exportedLevelASCII.txt");
+	myLevelLoader->LoadNewLevel("Levels/Dungeon_exportedLevelASCII.txt");
 	CScene* scene = CScene::GetInstance();
 	CLightFactory* lightFactory = CLightFactory::GetInstance();
 	CEnvironmentLight* environmentLight = lightFactory->CreateEnvironmentLight("Yokohama2.dds");
@@ -97,9 +97,18 @@ void CGame::Update()
 	UpdateCamera();
 
 	if (Input::GetInstance()->IsKeyPressed('1')) {
-		myLevelLoader->LoadNewLevel("Levels/SampleScene_exportedLevelASCII.txt");
+		myLevelLoader->LoadNewLevel("Levels/Dungeon_exportedLevelASCII.txt");
 	}
 	if (Input::GetInstance()->IsKeyPressed('2')) {
+		myLevelLoader->LoadNewLevel("Levels/Gardens_exportedLevelASCII.txt");
+	}
+	if (Input::GetInstance()->IsKeyPressed('3')) {
+		myLevelLoader->LoadNewLevel("Levels/Castle_exportedLevelASCII.txt");
+	}
+	if (Input::GetInstance()->IsKeyPressed('4')) {
+		myLevelLoader->LoadNewLevel("Levels/SampleScene_exportedLevelASCII.txt");
+	}
+	if (Input::GetInstance()->IsKeyPressed('5')) {
 		myLevelLoader->LoadNewLevel("Levels/SampleScene_exportedLevelASCII_Smily.txt");
 	}
 	UpdatePointLights();
