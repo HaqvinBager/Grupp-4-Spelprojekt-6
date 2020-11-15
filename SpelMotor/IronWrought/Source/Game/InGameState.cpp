5016cd5ea9d71d1058f41602d2b17e73b52d3ecd
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2020-11-15 22:34:16 +0100

    Added more examples of animated ui elements and increased usability. added json documents

--- SpelMotor/IronWrought/Source/Game/InGameState.cpp
+++ SpelMotor/IronWrought/Source/Game/InGameState.cpp
@@ -36,8 +36,15 @@ CInGameState::CInGameState(CStateStack& aStateStack) : CState(aStateStack) {
 	spriteInstance->SetPosition({ 0.0f,-0.82f });
 	CScene::GetInstance()->AddInstance(spriteInstance);
 
-	CAnimatedUIElement* uiElement = new CAnimatedUIElement();
+	CAnimatedUIElement* uiElement = new CAnimatedUIElement("VFXData_UI_HealthOrb.json");
+	uiElement->SetPosition({ -0.47f, -0.82f });
 	CScene::GetInstance()->AddInstance(uiElement);
+	CAnimatedUIElement* uiElement2 = new CAnimatedUIElement("VFXData_UI_ResourceOrb.json");
+	uiElement2->SetPosition({ 0.47f, -0.82f });
+	CScene::GetInstance()->AddInstance(uiElement2);
+	CAnimatedUIElement* uiElement3 = new CAnimatedUIElement("VFXData_UI_ExperienceBar.json");
+	uiElement3->SetPosition({ 0.0f, -0.735f });
+	CScene::GetInstance()->AddInstance(uiElement3);
 
 	//CGameObject* enemy = new CGameObject();
 	//enemy->AddComponent<CTransformComponent>(*enemy);
@@ -108,8 +115,12 @@ void CInGameState::Update()
 	}
 
 	std::vector<CSpriteInstance*> animatedUIFrames;
-	auto animUI = CScene::GetInstance()->CullAnimatedUI(animatedUIFrames)[0];
-	animUI->Level(abs(sin(CTimer::Time())));
+	auto animUI1 = CScene::GetInstance()->CullAnimatedUI(animatedUIFrames)[0];
+	animUI1->Level(abs(sin(CTimer::Time())));
+	auto animUI2 = CScene::GetInstance()->CullAnimatedUI(animatedUIFrames)[1];
+	animUI2->Level(abs(sin(CTimer::Time() - (3.1415f/2.0f))));
+	auto animUI3 = CScene::GetInstance()->CullAnimatedUI(animatedUIFrames)[2];
+	animUI3->Level(abs(sin(CTimer::Time() * 0.33f)));
 
 	CCollisionManager::GetInstance()->Update();
 
