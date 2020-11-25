05de6aa0e64ab54cf06cd9dfbed3dcebc8efda67
AxelSavage <axelsavage@gmail.com>
2020-11-25 13:02:05 +0100

    added NavMEsh Export into ExportAllbin and changed directory to Bin/Levels/

--- SpelMotor/UnityProject/Assets/Scripts/Navmeshexport.cs
+++ SpelMotor/UnityProject/Assets/Scripts/Navmeshexport.cs
@@ -1,26 +1,32 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
  
 // Obj exporter component based on: http://wiki.unity3d.com/index.php?title=ObjExporter
  
-public class ExportNavMeshToObj : MonoBehaviour {
+
+
+public class ExportNavMeshToObj {
  
-    [MenuItem("Custom/Export NavMesh to mesh")]
-    static void Export() {
+    static readonly string fileName = "..\\IronWrought\\Bin\\Levels\\";
+
+    [MenuItem("Custom/[Deprecated] Export NavMesh to mesh")]
+    public static void Export() {
         UnityEngine.AI.NavMeshTriangulation triangulatedNavMesh = UnityEngine.AI.NavMesh.CalculateTriangulation();
  
         Mesh mesh = new Mesh();
         mesh.name = "ExportedNavMesh";
         mesh.vertices = triangulatedNavMesh.vertices;
         mesh.triangles = triangulatedNavMesh.indices;
-        string filename = "..\\IronWrought\\Bin\\Levels\\NavMesh\\" + Path.GetFileNameWithoutExtension(EditorApplication.currentScene) + "_ExportedNavMesh.obj";
+        string filename = fileName + Path.GetFileNameWithoutExtension(EditorApplication.currentScene) + "_ExportedNavMesh.obj";
+        //"..\\IronWrought\\Bin\\Levels\\NavMesh\\" + Path.GetFileNameWithoutExtension(EditorApplication.currentScene) + "_ExportedNavMesh.obj";
         MeshToFile(mesh, filename);
-        print("NavMesh exported as '" + filename + "'");
+        Debug.Log("NavMesh exported as '" + filename + "'");
         AssetDatabase.Refresh();
     }
- 
+
     static string MeshToString(Mesh mesh) {
         StringBuilder sb = new StringBuilder();
  
@@ -55,3 +61,30 @@ public class ExportNavMeshToObj : MonoBehaviour {
         }
     }
 }
+
+//public struct NavMeshData
+//{
+//    public Vector3[] myVerticies;
+//    public Vector3[] myNormals;
+//    public Vector2[] myUVs;
+//    public int[] myTriangles;
+//}
+
+//static NavMeshData MeshToData(Mesh mesh)
+//{
+//    NavMeshData navMeshData = new NavMeshData();
+
+//    mesh.vertices.CopyTo(navMeshData.myVerticies, 0);
+//    mesh.normals.CopyTo(navMeshData.myNormals, 0);
+//    mesh.uv.CopyTo(navMeshData.myUVs, 0);
+
+//    List<int> triangles = new List<int>();
+//    for (int material = 0; material < mesh.subMeshCount; material++)
+//    {
+//        triangles.AddRange(mesh.GetTriangles(material));       
+//    }
+
+//    navMeshData.myTriangles = triangles.ToArray();
+
+//    return navMeshData;
+//}
