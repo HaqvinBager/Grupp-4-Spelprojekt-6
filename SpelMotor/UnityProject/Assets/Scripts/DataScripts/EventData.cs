b37ad6f72d6667d488362637909af8bc3d76fada
AlexanderMatthai <71022308+AlexanderMatthai@users.noreply.github.com>
2020-11-25 17:47:36 +0100

    added event data reading from unity

---
+++ SpelMotor/UnityProject/Assets/Scripts/DataScripts/EventData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Event
+{
+    LoadLevel
+}
+
+public struct EventData
+{
+    public Vector3 myPosition;
+    public Vector2 myColliderData;
+    public int myEvent;
+    public EventData(EventSetup eventSetup)
+    {
+        myPosition = eventSetup.transform.position;
+        myColliderData.x = eventSetup.myColliderData.x;
+        myColliderData.y = eventSetup.myColliderData.y;
+        myEvent = (int)eventSetup.myEvent;
+    }
+}
---
+++ SpelMotor/UnityProject/Assets/Scripts/EventSetup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class EventSetup : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector2 myColliderData;
+
+    public Event myEvent;
+    private void Update()
+    {
+       myColliderData.x = gameObject.GetComponent<BoxCollider>().size.x;
+       myColliderData.y = gameObject.GetComponent<BoxCollider>().size.z;
+    }
+    
+}
