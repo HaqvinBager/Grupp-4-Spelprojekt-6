25399ff605ba723d3ff83f474ae1004b8d3db41d
AxelSavage <axelsavage@gmail.com>
2020-12-08 14:38:33 +0100

    Updated JSON Files with new Names so they can be used with the Unity Pipeline Created new Scripts for exporting Json Paths from Unity to Engine, so they can be found by ParticleComponent and VFXComponent. Did some folder-structuring.

---
+++ SpelMotor/UnityProject/Assets/Scripts/Setups/EnvironmentFXSetup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public enum EnvironmentFXType
+{
+    Water,
+    CrackDaughter,
+    FogPlane
+}
+
+[System.Serializable]
+public struct VFXData
+{
+    public string myJson;
+    public bool useMe;
+}
+
+public class EnvironmentFXSetup : MonoBehaviour
+{
+    public EnvironmentFXType environmentFXType;
+
+    public List<VFXData> myJsonPaths = new List<VFXData>();
+
+    private string target_path = "..\\IronWrought\\Bin\\json\\";
+    
+    private void OnValidate()
+    {
+        JsonFinder.RefreshData(myJsonPaths, "VFXData_");
+        //string[] paths = Directory.GetFiles(target_path, "*.json");
+        ////myJsonPaths.Clear();
+        //foreach (string vfxData in paths)
+        //{
+        //    int startIndex = vfxData.IndexOf("VFX");
+        //    if(startIndex > 0)
+        //    {
+        //        bool foundNew = true;
+        //        string jsonName = vfxData.Substring(startIndex, vfxData.Length - startIndex);
+        //        if (jsonName.Contains("_UI"))
+        //        {
+        //            continue;
+        //        }
+
+        //        foreach(var data in myJsonPaths)
+        //        {
+        //            if (data.myJson.Equals(jsonName))
+        //            {
+        //                foundNew = false;
+        //            }
+        //        }
+        //        if (foundNew)
+        //        {
+        //            myJsonPaths.Add(new VFXData {myJson = jsonName });             
+        //        }
+        //    }
+        //}
+        //Debug.Log(environmentFXType.ToString());
+    }
+
+}
