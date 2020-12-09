d0c357d5ef85be3afe02f3b25dd63de094401f9c
AxelSavage <axelsavage@gmail.com>
2020-12-09 15:13:27 +0100

    Comment in script

--- SpelMotor/UnityProject/Assets/Scripts/Setups/BossSetup.cs
+++ SpelMotor/UnityProject/Assets/Scripts/Setups/BossSetup.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class BossSetup : MonoBehaviour
 {
+    //Editor Script Makes these show up as MinMax Sliders
     [HideInInspector] public Vector2 myStageOne;
     [HideInInspector] public Vector2 myStageTwo;
     [HideInInspector] public Vector2 myStageThree;
+
 }
