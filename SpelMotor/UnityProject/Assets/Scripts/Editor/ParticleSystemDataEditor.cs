eed54fe4d0c73e4276f80e5f636245446c9a5da4
AxelSavage <axelsavage@gmail.com>
2020-12-10 16:22:34 +0100

    Testing purposes: added particle data for testing in unity

---
+++ SpelMotor/UnityProject/Assets/Scripts/Editor/ParticleSystemDataEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(ParticleSystemData))]
+public class ParticleSystemDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        base.OnInspectorGUI();
+
+        string[] paths = JsonFinder.GetJsonPaths("VFXData_").ToArray();
+        int choice = serializedObject.FindProperty("myTexturePathIndex").intValue;
+        choice = EditorGUILayout.Popup("Texture", choice, paths);
+        serializedObject.FindProperty("myTexturePathIndex").intValue = choice;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ SpelMotor/UnityProject/Assets/Scripts/SetupData/ParticleSystemData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="FX/ParticleSystem")]
+public class ParticleSystemData : ScriptableObject
+{
+    public int myMaxNumberOfParticles;
+    public float myDelay;
+    public float myDuration;
+    public float mySpawnDuration;
+    public float myLifeTime;
+    public float mySpeed;
+    public Color myStartColor;
+    public Color myEndColor;
+    public Vector2 myStartEndSize;
+    public Vector3 myOffsetPosition;
+    public Vector3 myStartDirection;
+    public Vector3 myEndDirection;
+    public Vector2 myLiftimeLowerUpperBounds;
+    public Vector2 mySpeedLowerUpperBounds;
+    public Vector3 myDirectionLowerBound;
+    public Vector3 myDirectionUpperBound;
+    [HideInInspector] public int myTexturePathIndex;
+}
